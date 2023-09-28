e4aeed2d869d9126556249965d4ad4191c622e79
Nam G VU <namgivu@gmail.com>
2023-09-28 15:11:00 +0700

    bard to helpfix err

---
+++ s230926_1245_demo_bard_fix_error/demo_bard_fix_error00.py
@@ -0,0 +1,6 @@
+cuahang_d = {
+  'ten':'btx',
+
+print(cuahang_d)
+
+# ref bard fix https://g.co/bard/share/c11e1c52a1bd
---
+++ s230926_1245_demo_bard_fix_error/demo_bard_fix_error01.py
@@ -0,0 +1,8 @@
+cuahang_d = {
+  'ten': 'btx'
+  'gia': 2000
+}
+
+print(cuahang_d)
+
+# ref bard https://g.co/bard/share/458583740688
